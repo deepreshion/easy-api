5648210ad708a4d4435a8455c43bda2e718364d8
Ruslan <rowemo28@gmail.com>
2024-01-25 17:39:13 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import axios from 'axios'
 // import {errorNotify} from '@helpers/useNotify'
 
-const api = axios.create({
+let api = axios.create({
   baseURL: import.meta.env.VITE_APP_API_SERVICE,
 })
+
+let errorNotify = (error) => {
+    console.log(error);
+}
+
+const mountApi = (baseUrl: string, errorCallback: Function) => {
+    api = axios.create({
+        baseURL: baseUrl,
+    })
+    errorNotify = errorCallback
+}
 
 const useResponse: IUseResponse = async(data = null, options, errorMessage) => {
   let response: any = {}
